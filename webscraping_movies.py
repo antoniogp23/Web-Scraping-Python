0f82072d1d28ec2ea4f894e1d43decf7468ae546
antoniogp23 <gutierrezantonio2397@gmail.com>
2025-02-18 12:06:28 -0500

    Updated number of movies

--- webscraping_movies.py
+++ webscraping_movies.py
@@ -21,7 +21,7 @@ rows = tables[0].find_all('tr')
 
 #Iterate over the rows
 for row in rows:
-    if count<25:
+    if count<50:
         col = row.find_all('td')
         if len(col)!=0:
             data_dict = {"Average Rank": col[0].contents[0],
